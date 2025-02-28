cffb75ceedabc506e967f3eef577bd98a2abcea7
ANTONIO ROMEU MIGUEL DOS SANTOS NETO <antonio.rmsneto@senacsp.edu.br>
2025-02-27 21:17:10 -0300

    feat exercicio 2 corrigido  e atualizado

---
+++ Cex2/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Cex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu = @"----------------------------
+1-domingo
+2-segunda-feira
+3-terça-feira
+4-quarta-feira
+5-quinta-feira
+6-sexta-feira
+7-sábado 
+----------------------------
+";
+            Console.WriteLine(menu);
+            byte opção = byte.Parse(Console.ReadLine()!);
+
+            switch (opção) {
+                case 1:
+                    Console.WriteLine("domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("segunda-feira");
+                    break;
+                case 3:
+                    Console.WriteLine("terça-feira");
+                    break;
+                case 4:
+                    Console.WriteLine("quarta-feira");
+                    break;
+                case 5:
+                    Console.WriteLine("quinta-feira");
+                    break;
+                case 6:
+                    Console.WriteLine("sexta-feira");
+                    break;
+                case 7:
+                    Console.WriteLine("sábado");
+                    break;
+                case 8:
+                    break;
+            }
+    }
+}
---
+++ WFInicial/FormEx02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFInicial
+{
+    public partial class FormEx02 : Form
+    {
+        public FormEx02()
+        {
+            InitializeComponent();
+        }
+    }
+}
